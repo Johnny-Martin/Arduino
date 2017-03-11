f0ceed8965eba2a36bc02b6fff498f65abd9964b
Johnny-Martin <Johnny-Martin@users.noreply.github.com>
2017-03-11 20:58:59 +0800

    use the wheel control the left arrow and the right arrow

--- Keyboard/Keyboard.ino
+++ Keyboard/Keyboard.ino
@@ -1,18 +1,27 @@
+#include "Keyboard.h"
 #include"Encoder\Encoder.h"
 using namespace ATL;
 
 int count = 0;
 Encoder<11, 12> mouseEncoder;
 
-void setup() { 
+void setup() {
    Serial.begin (9600);
-   
+   pinMode(8, INPUT);
+   digitalWrite(8, HIGH);
+   Keyboard.begin();
 } 
 
- void loop() {
+void loop() {
 	if(mouseEncoder.Excute() > 0){
 		Serial.println(++count);
+		if(digitalRead(8) == LOW){
+			Keyboard.write(KEY_LEFT_ARROW);
+		}
 	}else if(mouseEncoder.Excute() < 0){
 		Serial.println(--count);
+		if(digitalRead(8) == LOW){
+			Keyboard.write(KEY_RIGHT_ARROW);
+		}
 	}
- } 
+} 
