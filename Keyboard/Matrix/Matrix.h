c8369013e6129918dff5eda6a6760451dc7c2204
Johnny-Martin <Johnny-Martin@users.noreply.github.com>
2017-03-12 22:40:36 +0800

    Matrix for programmer

--- Keyboard/Matrix/Matrix.h
+++ Keyboard/Matrix/Matrix.h
@@ -10,14 +10,14 @@ uint8_t Col[matrixCol] = {9,10,11,12,13,A0,A1,A2};
 
 //程序员专用，上方一行数字键默认为符号
 uint8_t Matrix[matrixRow][matrixCol] = {
-	{'2','3','4','5','7','8','9','='},
-	{'1','d','h','f','o','l','p','-'},
+	{'#','$','%','!','*','(',')','='},
+	{'@','d','h','f','o','l','p','-'},
 	{ESC,'b','e','u','n','i',',','~'},
-	{Tab,'0','s','a','w','r','.','\\'},
+	{Tab,'^','s','a','w','r','.','\\'},
 	{Fun,'y','x',0  ,bak,'g',';','\''},
 	{shift,'z','c',0,'}','k',upA,'/'},
 	{ctrl,alt,'v',0 ,'t','j',lfA,rtA},
-	{win,spc,'{','6','m','q',etr,btA}
+	{win,spc,'{','&','m','q',etr,btA}
 };
 
 uint8_t Matrix_Shift[matrixRow][matrixCol] = {
