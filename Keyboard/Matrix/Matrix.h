083e23ed66024ba3ba67c769c1dcd1b77919bdab
Johnny-Martin <Johnny-Martin@users.noreply.github.com>
2017-03-13 02:05:48 +0800

    add media key

--- Keyboard/Matrix/Matrix.h
+++ Keyboard/Matrix/Matrix.h
@@ -17,6 +17,16 @@ uint8_t Col[matrixCol] = {9,10,11,12,13,A0,A1,A2};
 #define KEY_RIGHT_ARROW  KEY_RARROW
 #define KEY_DOWN_ARROW   KEY_DARROW
 #define KEY_UP_ARROW     KEY_UARROW
+#define KEY_PAGEUP       0
+#define KEY_PAGEDOWN     0
+#define KEY_HOME         0
+#define KEY_END          0
+#define KEY_INSERT       0
+#define KEY_PRTSC        0
+#define KEY_BREAK        0
+
+#define KEY_VOLUP        0
+#define KEY_VOLDOWN      0
 
 #define KEY_FUN 0xFF
 #define KEY_SPACE ' '
@@ -46,13 +56,13 @@ uint8_t Matrix_Shift[matrixRow][matrixCol] = {
 };
 
 uint8_t Matrix_Fun[matrixRow][matrixCol] = {
-	{KEY_F2,    KEY_F3,    KEY_F4, KEY_F5, KEY_F7,  KEY_F8, KEY_F9,     '+'},
-	{KEY_F1,    'D',       'H',    'F',    '7',     '8',    '9',        '_'},
-	{KEY_ESC,   'B',       'E',    'U',    '4',     '5',    '<',        '`'},
+	{KEY_F2,    KEY_F3,    KEY_F4, KEY_F5, KEY_F7,  KEY_F8, KEY_F9,     KEY_F10},
+	{KEY_F1,    'D',       'H',    'F',    '7',     '8',    '9',        KEY_F11},
+	{KEY_ESC,   'B',       'E',    'U',    '4',     '5',    '<',        KEY_F12},
 	{KEY_TAB,   '0',       'S',    'A',    '1',     '6',    '>',        '|'},
 	{KEY_FUN,   'Y',       'X',     0 ,    KEY_DEL, 'G',    ':',        '\"'},
-	{KEY_SHIFT, 'Z',       'C',     0 ,    '0',     'K',    KEY_UARROW, '?'},
-	{KEY_CTRL,  KEY_ALT,   'V',     0 ,    'T',     '3',    KEY_LARROW, KEY_RARROW},
-	{KEY_WIN,   KEY_SPACE, '[',    KEY_F6, 'M',     '2',    KEY_ENTER,  KEY_DARROW}
+	{KEY_SHIFT, 'Z',       'C',     0 ,    '0',     'K',    KEY_PAGEUP, '?'},
+	{KEY_CTRL,  KEY_ALT,   'V',     0 ,    'T',     '3',    KEY_HOME,   KEY_END},
+	{KEY_WIN,   KEY_SPACE, '[',    KEY_F6, 'M',     '2',    KEY_ENTER,  KEY_PAGEDOWN}
 }
 #endif
