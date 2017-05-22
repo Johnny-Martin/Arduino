7207cb8f76e1c8dab6c62773b3f6893b7266daf6
Johnny-Martin <Johnny-Martin@users.noreply.github.com>
2017-05-22 19:07:47 +0800

    KeyboardProxy

--- Keyboard/KeyboardProxy/KeyboardProxy.h
+++ Keyboard/KeyboardProxy/KeyboardProxy.h
@@ -2,13 +2,16 @@
 #define _KEYBOARDPROXY_H_
 
 #include <stdint.h>
-#include "Matrix.h"
-#include "VirtualParallelPort.h"
+#include "..\Matrix\Matrix.h"
+#include "..\VirtualParallelPort\VirtualParallelPort.h"
 
 namespace EKEY{
 	//扫描键盘矩阵，并根据检测到的按键，结合Matrix.h中提供的键位定义，输出按键信息
 	class KeyboardProxy{
-		
+		VirtualParallelPort* rowPort;
+		VirtualParallelPort* columnPort;
+	public:
+		KeyboardProxy();
 	}
 }
 #endif
