51a7aa7464124d7eba6b2384fbb118fb99ed3d07
Johnny-Martin <Johnny-Martin@users.noreply.github.com>
2017-05-22 19:39:56 +0800

    基本结构

--- Keyboard/KeyboardProxy/KeyboardProxy.h
+++ Keyboard/KeyboardProxy/KeyboardProxy.h
@@ -2,16 +2,35 @@
 #define _KEYBOARDPROXY_H_
 
 #include <stdint.h>
+#include <Keyboard.h>
 #include "..\Matrix\Matrix.h"
 #include "..\VirtualParallelPort\VirtualParallelPort.h"
-
+/*
+ *
+ */
 namespace EKEY{
-	//扫描键盘矩阵，并根据检测到的按键，结合Matrix.h中提供的键位定义，输出按键信息
+	/*1,扫描键盘矩阵，并根据检测到的按键，结合Matrix.h中提供的键位定义，输出按键信息
+	 *2,扫描滚轮，并根据按键状态输出滚轮方向。滚轮默认为输出左右箭头；按住Fun输出上下箭头
+	 *因为Arduino的IO资源紧缺，无法使用两个滚轮。蓝牙版键盘IO充足的话会再加一个滚轮，
+	 *用作鼠标滚轮。
+	 */
 	class KeyboardProxy{
 		VirtualParallelPort* rowPort;
 		VirtualParallelPort* columnPort;
+		bool IsFunDbClicked();
+		bool IsFunDown();
+		
 	public:
-		KeyboardProxy();
+		KeyboardProxy(){
+			rowPort    = new VirtualParallelPort<8>(Row);
+			columnPort = new VirtualParallelPort<8>(Col);
+		}
+		
+		void Init(){
+			Keyboard.begin();
+		}
+		
+		void Excute(void);
 	}
 }
 #endif
