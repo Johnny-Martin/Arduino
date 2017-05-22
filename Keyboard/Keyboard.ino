c7497cddb055a45733a646e6403914cc675956a5
Johnny-Martin <Johnny-Martin@users.noreply.github.com>
2017-05-23 00:57:01 +0800

    编译

--- Keyboard/Keyboard.ino
+++ Keyboard/Keyboard.ino
@@ -1,8 +1,9 @@
+#include <stdint.h>
 #include "KeyboardProxy\KeyboardProxy.h"
 
 using namespace EKEY;
 
-KeyboardProxy kbd{};
+KeyboardProxy kbd;
 void setup() {
 	Serial.begin (9600);
 	kbd.Init();
