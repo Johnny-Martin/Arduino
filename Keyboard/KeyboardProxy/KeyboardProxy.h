47efe9327d4701c4d5403f63679db9c5b8186756
Johnny-Martin <Johnny-Martin@users.noreply.github.com>
2017-05-22 17:14:31 +0800

    使用目录

---
+++ Keyboard/KeyboardProxy/KeyboardProxy.h
@@ -0,0 +1,12 @@
+#ifndef _KEYBOARDPROXY_H_
+#define _KEYBOARDPROXY_H_
+
+#include <stdint.h>
+#include "Matrix.h"
+#include "VirtualParallelPort.h"
+
+//扫描键盘矩阵，并根据检测到的按键，结合Matrix.h中提供的键位定义，输出按键信息
+class KeyboardProxy{
+	
+}
+#endif
