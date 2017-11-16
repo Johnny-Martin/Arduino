ba41a2157083eb8c6b901711e981c7b1c1684e57
Johnny Martin <970803555@qq.com>
2017-11-16 20:04:32 +0800

    删除无用代码

--- Keyboard/Matrix/Matrix.h
+++ Keyboard/Matrix/Matrix.h
@@ -197,51 +197,5 @@ namespace EKEY{
 		0		// DEL
 	};
 
-	//程序员专用，上方一行数字键默认为符号
-	/*
-	uint8_t Matrix[matrixRow][matrixCol] = {
-		{'#',       '$',       '%', '!', '*',           '(', ')',        '='},
-		{'@',       'd',       'h', 'f', 'o',           'l', 'p',        '-'},
-		{KEY_ESC,   'b',       'e', 'u', 'n',           'i', ',',        '~'},
-		{KEY_TAB,   '^',       's', 'a', 'w',           'r', '.',        '\\'},
-		{KEY_FUN,   'y',       'x',  0 , KEY_BACKSPACE, 'g', ';',        '\''},
-		{KEY_SHIFT, 'z',       'c',  0 , '}',           'k', KEY_UARROW, '/'},
-		{KEY_CTRL,  KEY_ALT,   'v',  0 , 't',           'j', KEY_LARROW, KEY_RARROW},
-		{KEY_WIN,   KEY_SPACE, '{', '&', 'm',           'q', KEY_ENTER,  KEY_DARROW}
-	};
-
-	uint8_t Matrix_Shift[matrixRow][matrixCol] = {
-		{'2',       '3',       '4', '5', '7',           '8', '9',        '+'},
-		{'1',       'D',       'H', 'F', 'O',           'L', 'P',        '_'},
-		{KEY_ESC,   'B',       'E', 'U', 'N',           'I', '<',        '`'},
-		{KEY_TAB,   '0',       'S', 'A', 'W',           'R', '>',        '|'},
-		{KEY_FUN,   'Y',       'X',  0 , KEY_DEL,       'G', ':',        '\"'},
-		{KEY_SHIFT, 'Z',       'C',  0 , ']',           'K', KEY_UARROW, '?'},
-		{KEY_CTRL,  KEY_ALT,   'V',  0 , 'T',           'J', KEY_LARROW, KEY_RARROW},
-		{KEY_WIN,   KEY_SPACE, '[', '6', 'M',           'Q', KEY_ENTER,  KEY_DARROW}
-	};
-
-	uint8_t Matrix_Fun[matrixRow][matrixCol] = {
-		{KEY_F2,    KEY_F3,    KEY_F4, KEY_F5, KEY_F7,  KEY_F8, KEY_F9,     KEY_F10},
-		{KEY_F1,    'D',       'H',    'F',    '7',     '8',    '9',        KEY_F11},
-		{KEY_ESC,   'B',       'E',    'U',    '4',     '5',    '<',        KEY_F12},
-		{KEY_TAB,   '0',       'S',    'A',    '1',     '6',    '>',        '|'},
-		{KEY_FUN,   'Y',       'X',     0 ,    KEY_DEL, 'G',    ':',        '\"'},
-		{KEY_SHIFT, 'Z',       'C',     0 ,    '0',     'K',    KEY_PAGEUP, '?'},
-		{KEY_CTRL,  KEY_ALT,   'V',     0 ,    'T',     '3',    KEY_HOME,   KEY_END},
-		{KEY_WIN,   KEY_SPACE, '[',    KEY_F6, 'M',     '2',    KEY_ENTER,  KEY_PAGEDOWN}
-	};
-	
-	uint8_t Matrix_DbFun[matrixRow][matrixCol] = {
-		{KEY_F2,    KEY_F3,    KEY_F4, KEY_F5, KEY_F7,  KEY_F8, KEY_F9,     KEY_F10},
-		{KEY_F1,    'D',       'H',    'F',    '7',     '8',    '9',        KEY_F11},
-		{KEY_ESC,   'B',       'E',    'U',    '4',     '5',    '<',        KEY_F12},
-		{KEY_TAB,   '0',       'S',    'A',    '1',     '6',    '>',        '|'},
-		{KEY_FUN,   'Y',       'X',     0 ,    KEY_DEL, 'G',    ':',        '\"'},
-		{KEY_SHIFT, 'Z',       'C',     0 ,    '0',     'K',    KEY_PAGEUP, '?'},
-		{KEY_CTRL,  KEY_ALT,   'V',     0 ,    'T',     '3',    KEY_HOME,   KEY_END},
-		{KEY_WIN,   KEY_SPACE, '[',    KEY_F6, 'M',     '2',    KEY_ENTER,  KEY_PAGEDOWN}
-	};
-	*/
 }
 #endif
